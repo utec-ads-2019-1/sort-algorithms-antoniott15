84f72bdf39452a23fe98750dee170bc574f1603a
Antonio <antonio.toche@utec.edu.pe>
2019-04-06 23:43:33 -0500

    select and insert added: done

--- tester.cpp
+++ tester.cpp
@@ -1,28 +1,81 @@
 #include "tester.h"
 
-Sort* Tester::getSort(Algorithm sort, void *array, size_t size) {
-    switch (sort) {
-        case bubblesort: return new BubbleSort(array, size);
-        case selectsort: return new SelectSort(array, size);
-        case insertsort: return new InsertSort(array, size);
-        case shellsort: return new ShellSort(array, size);
-        case quicksort: return new QuickSort(array, size);
-        case mergesort: return new MergeSort(array, size);
-        default: throw invalid_argument("Not a valid sort");
+void integersBubble(void *elements, int first, int second)
+{
+    int *array = (int *)elements;
+    if (array[first] > array[second])
+    {
+        swap(array[first], array[second]);
     }
 }
 
-void Tester::integerSorts(int *array, size_t size, void (*compare)(void*, int, int)) {
-    Sort* sort;
+void integersInsert(void *elements, int first, int second)
+{
+    int *array = (int *)elements;
+    if (array[first] > array[second])
+    {
+        swap(array[first], array[second]);
+    }
+}
+
+void integersSelect(void *elements, int first, int second)
+{
+    int *array = (int *)elements;
+    if (second != first)
+    {
+        swap(array[first], array[second]);
+    }
+}
+
+Sort *Tester::getSort(Algorithm sort, void *array, size_t size)
+{
+    switch (sort)
+    {
+    case bubblesort:
+        return new BubbleSort(array, size);
+    case insertsort:
+        return new InsertSort(array, size);
+    case selectsort:
+        return new SelectSort(array, size);
+    /* case shellsort:
+        return new ShellSort(array, size);
+    case quicksort:
+        return new QuickSort(array, size);
+    case mergesort:
+        return new MergeSort(array, size);*/
+    default:
+        throw invalid_argument("Not a valid sort");
+    }
+}
+
+fptr Tester::getCompare(Algorithm sort)
+{
+    switch (sort)
+    {
+    case bubblesort:
+        return &integersBubble;
+    case insertsort:
+        return &integersInsert;
+    case selectsort:
+        return &integersSelect;
+    default:
+        throw invalid_argument("Not a valid comparer");
+    }
+}
+
+void Tester::integerSorts(int *array, size_t size)
+{
+    Sort *sort;
     int temp[size];
 
-    Algorithm algorithm[] = { bubblesort, selectsort, insertsort, shellsort, quicksort, mergesort };
+    Algorithm algorithm[] = {bubblesort, insertsort, selectsort /*, shellsort, quicksort, mergesort*/};
     size_t numberOfAlgorithms = sizeof(algorithm) / sizeof(algorithm[0]);
 
-    for (int i = 0; i < numberOfAlgorithms; i++) {
+    for (int i = 0; i < numberOfAlgorithms; i++)
+    {
         copy(array, array + size, temp);
         sort = getSort(algorithm[i], temp, size);
-        sort->execute(compare);
+        sort->execute(getCompare(algorithm[i]));
         ASSERT(is_sorted(temp, temp + size), "The " + sort->name() + " is not ordering all the elements");
     }
 }
